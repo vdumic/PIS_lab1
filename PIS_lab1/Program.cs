ca9bbc2c3b3e3173fcffd5f3da3d43aa0efff660
vdumic <vdumic00@fesb.hr>
2022-03-15 17:51:34 +0100

    Prvi commit

---
+++ PIS_lab1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PIS_lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
